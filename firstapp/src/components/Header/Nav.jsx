c0a9e3b94700d20241a5fef6b73a2afe4ab32d2c
Alisher <alisherxujanov163@gmail.com>
2023-07-29 19:56:15 +0500

    second lesson

--- firstapp/src/components/Header/Nav.jsx
+++ firstapp/src/components/Header/Nav.jsx
@@ -1,7 +1,12 @@
+import style from './header.scss'
+
 function Nav() {
     return (
-        <nav>
-            
+        <nav style={style}>
+            <a id="logo" href="#">Logo</a>
+            <a href="#">Contacts</a>
+            <a href="#">About</a>
+            <a href="#">Profile</a>
         </nav>
     );
 }
