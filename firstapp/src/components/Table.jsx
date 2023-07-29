774f8d068e5855f89963537aa05ea343acf4beed
Alisher <alisherxujanov163@gmail.com>
2023-07-29 18:39:06 +0500

    latest commit

---
+++ firstapp/src/components/Table.jsx
@@ -0,0 +1,45 @@
+function Table() {
+    return (
+        <>
+            <table border={1} bordercolor='red'>
+                <tr>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                </tr>
+                <tr>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                </tr>
+                <tr>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                </tr>
+                <tr>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                </tr>
+                <tr>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                    <th>Text</th>
+                </tr>
+            </table>
+        </>
+    );
+}
+
+export default Table;
