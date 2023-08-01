9a73d7d80d6b540f962828ba3598f7de11a4e60f
Alisher <alisherxujanov163@gmail.com>
2023-08-01 19:43:55 +0500

    small

--- firstapp/src/components/Header/Nav.jsx
+++ firstapp/src/components/Header/Nav.jsx
@@ -1,9 +1,12 @@
 import style from './header.scss'
 import Theme from '../Theme'
+import { useState } from 'react'
 
 function Nav() {
+    const [theme, setTheme] = useState('light')
+
     return (
-        <nav style={style}>
+        <nav className={theme ? "dark-theme" : "light-theme"} style={style}>
             <a id="logo" href="#">Logo</a>
 
             <a href="#">Contacts</a>
