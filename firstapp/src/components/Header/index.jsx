19b667e3ebb0a3cd0a91663346eb3bb1ebdf7d92
Alisher <alisherxujanov163@gmail.com>
2023-08-05 19:42:33 +0500

    latest commit

--- firstapp/src/components/Header/index.jsx
+++ firstapp/src/components/Header/index.jsx
@@ -3,10 +3,14 @@ import "./header.scss"
 
 const Header = () => {
 
+    let headingStyle = { 
+        color: "red", 
+    }
+
     return (
-        <div>
+        <div id='peak'>
             <header>
-                <h1>Header Text</h1>
+                <h1 style={headingStyle}>Header Text</h1>
             </header>
 
             <Nav />
