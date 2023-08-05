797141ee73bbc115bdda1cd1660d37308b8adafc
Alisher <alisherxujanov163@gmail.com>
2023-08-05 18:39:28 +0500

    router-dom

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   const lorem = "Lorem ipsum dolor sit amet con"
@@ -8,7 +9,18 @@ function App() {
       <Header test={lorem}>
         <h2>All children</h2>
       </Header>
-      
+
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={""} >
+            <Route index element={""} />
+            <Route path="about" element={""} />
+            <Route path="*" element={""} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
