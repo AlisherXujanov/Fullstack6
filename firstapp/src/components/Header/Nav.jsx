d7cc61a296ffbdf6e0ba1f32ff8129a5464b209f
Alisher <alisherxujanov163@gmail.com>
2023-08-05 18:19:04 +0500

    link-audio

--- firstapp/src/components/Header/Nav.jsx
+++ firstapp/src/components/Header/Nav.jsx
@@ -1,10 +1,16 @@
 import style from './header.scss'
 import Theme from '../Theme'
 import audioFile from '../../click.mp3'
+import linkAudioFile from '../../link_click.mp3'
 import { useState } from 'react'
 
 function Nav() {
     const [theme, setTheme] = useState(false)
+
+    function playAudio() {
+        const audio = new Audio(linkAudioFile)
+        audio.play()
+    }
 
     function changeTheme() {
         const audio = new Audio(audioFile)
@@ -16,9 +22,9 @@ function Nav() {
         <nav className={theme ? "dark-theme" : "light-theme"} style={style}>
             <a id="logo" href="#">Logo</a>
 
-            <a href="#">Contacts</a>
-            <a href="#">About</a>
-            <a href="#">Profile</a>
+            <a href="#" onClick={playAudio}>Contacts</a>
+            <a href="#" onClick={playAudio}>About</a>
+            <a href="#" onClick={playAudio}>Profile</a>
             <a id='theme-link' href="#" onClick={changeTheme}>
                 <Theme theme={theme} />
             </a>
