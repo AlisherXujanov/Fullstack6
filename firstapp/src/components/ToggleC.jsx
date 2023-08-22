06eecb32cb8957132b97ba2f762650972578d62d
Alisher <alisherxujanov163@gmail.com>
2023-08-22 19:19:51 +0500

    useReducer

--- firstapp/src/components/ToggleC.jsx
+++ firstapp/src/components/ToggleC.jsx
@@ -1,26 +1,50 @@
 import React, { useState, useReducer } from 'react'
 import Button from 'react-bootstrap/Button'
-import Test from './Test.jsx'
+
+
+const initialState = { 
+    count: 0,
+    loading: false,
+    error: ''
+}
+
+function reducerFunction(state, action) {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 }
+        case 'decrement':
+            return { count: state.count - 1 }
+        default:
+            throw new Error("Unexpected action")
+    }
+}
+
 
 export default function ToggleC() {
-    const [count, setCounter] = useState(0)
+    // const [count, setCounter] = useState(0)
+
+    // Here we want to convert this useState counter into useReducer counter
+    const [state, dispatch] = useReducer(reducerFunction, initialState)
+
+    function increment() {
+        dispatch({ type: 'increment' })
+    }
+    function decrement() {
+        dispatch({ type: 'decrement' })
+    }
 
     return (
         <div>
-            <Test />
-            <hr />
-
-
-            <h2>Count: {count}</h2>
-            <Button 
+            <h2>Count: {state.count}</h2>
+            <Button
                 variant='success'
-                onClick={() => setCounter(count + 1)}
+                onClick={increment}
             >
                 Increment +
             </Button>
-            <Button 
+            <Button
                 variant='danger'
-                onClick={() => setCounter(count - 1)}
+                onClick={decrement}
             >
                 Decrement -
             </Button>
