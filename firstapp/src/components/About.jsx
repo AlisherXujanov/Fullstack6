5642a9327c83ebbe9930c8212c0e4ee9784437ee
Alisher <alisherxujanov163@gmail.com>
2023-08-22 19:30:58 +0500

    buttons

--- firstapp/src/components/About.jsx
+++ firstapp/src/components/About.jsx
@@ -1,5 +1,7 @@
 import { motion } from 'framer-motion'
 import ToggleC from './ToggleC.jsx'
+import img from '../Assets/Images/1.jpg'
+import Button from 'react-bootstrap/Button'
 
 function About() {
     const initialMotion = { transform: 'scale(-1)' }
@@ -15,6 +17,16 @@ function About() {
             <p>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Текста, продолжил свое большой домах маленькая своих маленький использовало вершину путь, жаренные по всей. Образ путь живет все. Заголовок, буквенных собрал.</p>
             <hr />
             <ToggleC />
+            <hr />
+            <div style={{textAlign: 'center'}}>
+                <img src={img} width={100} height={100} /> <br />
+                <Button variant='success'>
+                    Left
+                </Button>
+                <Button variant='primary'>
+                    Right
+                </Button>
+            </div>
         </motion.div>
     );
 }
