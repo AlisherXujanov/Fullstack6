020c94bd523d50e6254e7a16b38474b741f99f65
Alisher <alisherxujanov163@gmail.com>
2023-08-24 19:30:12 +0500

    global store

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,17 +1,24 @@
 import { BrowserRouter } from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css"
 import AnimatedComponents from './components/Navigation/AnimatedComponents';
+import { useContext, useState } from 'react';
+import { context, initialState } from './Store';
+
 
 function App() {
   // useLocation is a hook that returns the location object that 
   // represents the current URL. You can think about it like a 
   // useState that returns a new location whenever the URL changes.
+  const [state, setState] = useState(initialState)
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <AnimatedComponents />
-      </BrowserRouter>
-    </div>
+    <context.Provider>
+      <div className="App">
+        <BrowserRouter>
+          <AnimatedComponents />
+        </BrowserRouter>
+      </div>
+    </context.Provider>
   );
 }
 
--- firstapp/src/Store.js
+++ firstapp/src/Store.js
@@ -1,2 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createContext } from 'react';
+
+const context = createContext()
+const initialState = {
+    darkTheme: false,
+}
+
+export {
+    context, 
+    initialState
+};
 
