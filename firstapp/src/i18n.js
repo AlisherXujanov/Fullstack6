4745b25890ddd84ecae94c60bf387a9da7a340ea
Alisher <alisherxujanov163@gmail.com>
2023-09-05 18:44:38 +0500

    index js

--- firstapp/src/i18n.js
+++ firstapp/src/i18n.js
@@ -1,8 +1,14 @@
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
+import enJSON from "./locales/en.json";
+import ruJSON from "./locales/ru.json";
 
 i18n.use(initReactI18next).init({
-    resources: {}, // Where we're gonna put translations' files
+    resources: {
+        en: { ...enJSON },
+        ru: { ...ruJSON },
+    }, // Where we're gonna put translations' files
     lng: "en",     // Set the initial language of the App
     fallbackLng: "en", // If the language detector fails, it will use this language
 });
+export default i18n;
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -4,10 +4,15 @@ import App from './App';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
